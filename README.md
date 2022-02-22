39375b3ca5677448a702a04d723df9e0db6fba32
Aslan15 <31630572+Aslan15@users.noreply.github.com>
2022-02-22 10:59:14 -0600

    Environment Set Up

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # DMASpy
 Decentralized Multi-Agent Satellite Simulator
+
+## Mac Install
+Create virtual environment
+```
+python -m venv .venv
+```
+
+Activate virtual environment
+```
+source .venv/bin/activate
+```
+
+Install dependencies
+```
+pip install -r requirements.txt
+```
--- src/test.py
+++ src/test.py
@@ -1,16 +1,1 @@
-from astropy import units as u
-
-from poliastro.bodies import Earth, Mars, Sun
-from poliastro.twobody import Orbit
-
-# Data for Mars at J2000 from JPL HORIZONS
-a = 1.523679 * u.AU
-ecc = 0.093315 * u.one
-inc = 1.85 * u.deg
-raan = 49.562 * u.deg
-argp = 286.537 * u.deg
-nu = 23.33 * u.deg
-
-orb = Orbit.from_classical(Sun, a, ecc, inc, raan, argp, nu)
-orb.propagate(3*u.day)
-orb.plot()
+print("Hello World!")
