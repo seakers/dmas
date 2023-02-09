ad890053c0d095d3a612a1988a7546f77ef161cd
aslan15 <31630572+Aslan15@users.noreply.github.com>
2023-02-09 15:36:01 -0600

    multi threading attempt

--- test/mp_prototype.py
+++ test/mp_prototype.py
@@ -7,13 +7,16 @@ import zmq.asyncio as azmq
 import multiprocessing as mp
 from multiprocessing import Pool, Process
 
+"""
+ASYNC
+"""
 async def server():
     try:
-        print('SERVER START')
-
+        print("SERVER: creating to hello world server socket...") 
         context = azmq.Context()
         socket = context.socket(zmq.REP)
-        socket.bind("tcp://*:5555")
+        socket.bind("tcp://*:5556")        
+        print("SERVER: socket established!") 
 
         m = 0
         while m < 10:
@@ -35,21 +38,24 @@ async def server():
 
             m += 1
         
-        print('SERVER DONE')
+        context.destroy()
+        print('SERVER: finished receving messages.')
 
     except asyncio.CancelledError:
-        print ('SERVER INTERRUPTED')
+        print ('SERVER: listnening INTERRUPTED')
+        context.destroy()
         return
 
 async def client():
     try:
-        print("SERVER: Connecting to hello world server...") 
+        print("CLIENT: Connecting to hello world server...") 
         context = azmq.Context()
         socket = context.socket(zmq.REQ)
-        socket.connect("tcp://localhost:5555")
-        print("SERVER: Connection established!") 
+        socket.connect("tcp://localhost:5556")
+        print("CLIENT: Connection established!") 
 
         #  Do 10 requests, waiting each time for a response
+        t_o = time.perf_counter()
         for _ in range(10):
             dst : str = 'SERVER'
             content_dict = dict()
@@ -64,14 +70,18 @@ async def client():
 
             #  Get the reply.
             src, content = await socket.recv_multipart()
-            t = time.perf_counter()
+            t_f = time.perf_counter()
             src = src.decode('ascii')
             content = content.decode('ascii')
                             
-            print(f"CLIENT: Received reply from {dst} at {t} [s]: {content}")
+            print(f"CLIENT: Received reply from {dst} at {t_f - t_o} [s]: {content}")
+
+        context.destroy()
+        print(f"CLIENT: finished sending messages to the server.")
 
     except asyncio.CancelledError:
         print('Client service cancelled.')
+        context.destroy()
         return
 
 def tough_process(n : int = 10000):
@@ -90,8 +100,12 @@ def tough_process(n : int = 10000):
 def run_client():
     asyncio.run(client())
 
-async def interrupter(n : float, src : str):
+def run_server():
+    asyncio.run(server())
+
+async def interruptor(n : float, src : str):
     try:
+        print(f'{src}: interruptor waiting for {n} [s]...')
         await asyncio.sleep(n, src)
         print(f'{src}: INTERRUPTOR TRIGGERED!')
     except asyncio.CancelledError:
@@ -99,79 +113,15 @@ async def interrupter(n : float, src : str):
         return
 
 def run_interruptor(n : float, src : str):
-    asyncio.run(interrupter(n, src))
-
-async def client_main():
-    print('CLIENT START')
-
-    # t1 = asyncio.create_task(client(), name='client')
-    # t2 = asyncio.create_task(multitough(), name='multitough')
-    # t3 = asyncio.create_task(interrupter(1), name='interrupter')
-
-    # done, pending = await asyncio.wait([t3], return_when=asyncio.FIRST_COMPLETED)
-
-    # for task in done:
-    #     task : asyncio.Task
-    #     print(f'{task.get_name()} completed!')
-
-    # for task in pending:
-    #     task : asyncio.Task
-    #     print(f'cancelling {task.get_name()}...')
-    #     task.cancel()
-    #     await task
-    #     print(f'{task.get_name()} cancelled!')
-
-    # if not t2.done():
-    #     print(f'cancelling {t2.get_name()}...')
-    #     t2.cancel()
-    #     await t2
-    #     print(f'{t2.get_name()} cancelled!')
-    
-    # try:
-    #     ctx = mp.get_context('spawn')
-
-    #     p1 = ctx.Process(target=tough_process, args=(10000000,))
-    #     p2 = ctx.Process(target=tough_process, args=(10000000,))
-    #     p3 = ctx.Process(target=run_client, args=())
-    #     p4 = ctx.Process(target=run_interruptor, args=(1,))
-
-    #     # p1.daemon = True
-    #     # p1.daemon = True
-
-    #     p1.start()
-    #     p2.start()
-    #     p3.start()
-    #     p4.start()
-
-    #     p4.join()
-
-    # except asyncio.CancelledError: 
-    #     print('terminating tough processes!')
-    
-    # finally:
-    #     p1.terminate()
-    #     p2.terminate()
-    #     p3.terminate()
-
-    #     print('CLIENT DONE')
-
-    pool = mp.Pool()
-    
-    pool.apply_async(tough_process, args = (10000000, ))
-    pool.apply_async(tough_process, args = (10000000, ))
-    pool.apply_async(run_client, args = ())
-    pool.apply(run_interruptor, args = (2, 'CLIENT'))
-
-    pool.terminate()
-    pool.join()   
-
-    print('CLIENT DONE')
+    asyncio.run(interruptor(n, src))
 
 async def server_main():
-    t1 = asyncio.create_task(server(), name='server')
-    t2 = asyncio.create_task(interrupter(10, 'SERVER'), name='interruptor')
+    print('SERVER: START')
 
-    done, pending = await asyncio.wait([t1, t2], return_when=asyncio.FIRST_COMPLETED)
+    t1 = asyncio.create_task(server(), name='server')
+    # t2 = asyncio.create_task(interruptor(10, 'SERVER'), name='interruptor')
+
+    done, pending = await asyncio.wait([t1], return_when=asyncio.FIRST_COMPLETED)
 
     for task in done:
         task : asyncio.Task
@@ -184,11 +134,33 @@ async def server_main():
         await task
         print(f'SERVER: {task.get_name()} cancelled!')
 
+    # pool = mp.Pool()
+    
+    # pool.apply_async(run_server, args = ())
+    # pool.apply(run_interruptor, args = (10, 'SERVER'))
+
+    # pool.terminate()
+    # pool.join()   
+
+    print('SERVER: DONE')
+
 def server_run():
     asyncio.run(server_main())
 
 def client_run():
-    asyncio.run(client_main())
+    print('CLIENT: START')
+
+    pool = mp.Pool(processes=4)
+    
+    pool.apply_async(tough_process, args = (10000000, ))
+    pool.apply_async(tough_process, args = (10000000, ))
+    pool.apply_async(run_client, args = ())
+    pool.apply(run_interruptor, args = (10, 'CLIENT'))
+
+    pool.terminate()
+    pool.join()   
+
+    print('CLIENT: DONE')
 
 if __name__ == "__main__":
     # goal: show that listening and actions occur in parallel           
@@ -197,3 +169,42 @@ if __name__ == "__main__":
 
     p1.start()
     p2.start()
+
+
+# def server():
+#     print('SERVER START')
+
+#     context = zmq.Context()
+#     socket = context.socket(zmq.REP)
+#     socket.bind("tcp://*:5555")
+
+#     m = 0
+#     while m < 10:
+#         #  Wait for next request from client
+#         print('SERVER: waiting on message...')
+#         dst, content = socket.recv_multipart()
+#         dst = dst.decode('ascii')
+#         content = json.loads(content.decode('ascii'))
+#         src : str = content['src']
+#         print(f"SERVER: Received request from {src}: {content}")
+
+#         #  Do some 'work'
+#         await asyncio.sleep(0.5)
+
+#         #  Send reply back to client
+#         src : str = content['src']
+#         resp : str = 'hello!'
+#         socket.send_multipart([src.encode('ascii'), resp.encode('ascii')])
+
+#         m += 1
+    
+#     print('SERVER DONE')
+
+
+# if __name__ == "__main__":
+#     # goal: show that listening and actions occur in parallel           
+#     p1 = Process(target=client_run)
+#     p2 = Process(target=server_run)  
+
+#     p1.start()
+#     p2.start()
