c4c44b0e41d0e17b03be4e50e6ad17a915e4d964
Alan Aguilar <aguilaraj15@tamu.edu>
2023-03-03 14:07:13 -0600

    message distribution unit tester

--- tests/test_network.py
+++ tests/test_network.py
@@ -1,5 +1,6 @@
 import json
 import random
+import time
 import unittest
 from tqdm import tqdm
 
@@ -97,6 +98,10 @@ class TestNetworkElement(unittest.TestCase):
         EXT = 'EXTERNAL'
 
     class DummyNetworkElement(NetworkElement):
+        def __init__(self, element_name: str, network_config: NetworkConfig, level: int = logging.INFO, logger: logging.Logger = None) -> None:
+            super().__init__(element_name, network_config, level, logger)
+            self.msgs = []
+
         def _external_sync(self) -> dict:
             return dict()
 
@@ -106,15 +111,12 @@ class TestNetworkElement(unittest.TestCase):
         def activate(self) -> dict:
             self._external_socket_map, self._internal_socket_map = self.config_network()
 
-            print(f'{self.name}: Internal Addresses:', self._network_config.get_internal_addresses()) 
-            print(f'{self.name}: External Addresses:', self._network_config.get_external_addresses()) 
-
         @abstractmethod
         async def routine():
             pass
 
         async def main(self):
-                timeout_task = asyncio.create_task(asyncio.sleep(15))
+                timeout_task = asyncio.create_task(asyncio.sleep(5))
                 coroutine_task = asyncio.create_task(self.routine())
 
                 _, pending = await asyncio.wait([timeout_task, coroutine_task], return_when=asyncio.FIRST_COMPLETED)
@@ -131,6 +133,13 @@ class TestNetworkElement(unittest.TestCase):
             finally:
                 self._deactivate_network()
 
+    class ReceiverKillMessage(SimulationMessage):
+        """
+        Dummy kill message
+        """
+        def __init__(self, src: str, dst: str, msg_type: str, id: str = None):
+            super().__init__(src, dst, msg_type, id)
+
     class Sender(DummyNetworkElement):
         def __init__(self, t_type, socket_type : zmq.SocketType, port : int, n :int, level=logging.INFO) -> None:
             network_name = 'TEST_NETWORK'    
@@ -138,9 +147,15 @@ class TestNetworkElement(unittest.TestCase):
                 internal_address_map = {socket_type: [f'tcp://*:{port}']}
                 external_address_map = dict()
 
+                if socket_type is not zmq.PUB:
+                    internal_address_map[zmq.PUB] = [f'tcp://*:{port+1}']
+
             elif t_type is TestNetworkElement.TransmissionTypes.EXT:
                 internal_address_map = dict()
                 external_address_map = {socket_type: [f'tcp://*:{port}']}
+
+                if socket_type is not zmq.PUB:
+                    external_address_map[zmq.PUB] = [f'tcp://*:{port+1}']
 
             network_config = NetworkConfig(network_name, internal_address_map, external_address_map)
             
@@ -152,15 +167,15 @@ class TestNetworkElement(unittest.TestCase):
 
         async def routine(self):
             try:
+                dt = 0.01
+                await asyncio.sleep(dt*10)
                 for _ in tqdm (range (self.n), desc="SENDER: Transmitting..."):
-                    dt = 0.01
                     await asyncio.sleep(dt)
 
                     src = self.name
                     dst = self.get_network_name()
-                    msg_type = 'TEST'
 
-                    msg = SimulationMessage(src, dst, msg_type)
+                    msg = SimulationMessage(src, dst, 'TEST')
                     self._log(f'sending message through port of type {self.socket_type}...')
                     if self.t_type is TestNetworkElement.TransmissionTypes.INT:   
                         status = 'successful!' if await self._send_internal_msg(msg, self.socket_type) else 'failed.'
@@ -170,39 +185,96 @@ class TestNetworkElement(unittest.TestCase):
                     self.msgs.append(msg)
                     self._log(f'finished sending message! Transmission status: {status}')
 
+                src = self.name
+                dst = self.get_network_name()
+                kill_msg = TestNetworkElement.ReceiverKillMessage(src, dst, 'KILL')
+                if self.t_type is TestNetworkElement.TransmissionTypes.INT:   
+                    status = 'successful!' if await self._send_internal_msg(kill_msg, zmq.PUB) else 'failed.'
+
+                elif self.t_type is TestNetworkElement.TransmissionTypes.EXT:
+                    status = 'successful!' if await self._send_external_msg(kill_msg, zmq.PUB) else 'failed.'
+
+                self._log(f'finished sending messages! Kill message transmission status: {status}')
+
             except asyncio.CancelledError:
                 return
 
     class Receiver(DummyNetworkElement):
-        def __init__(self, t_type, socket_type : zmq.SocketType, port : int, n :int, level=logging.INFO) -> None:
+        def __init__(self, name, t_type, socket_type : zmq.SocketType, port : int, n :int, level=logging.INFO) -> None:
             network_name = 'TEST_NETWORK'     
 
             if t_type is TestNetworkElement.TransmissionTypes.INT:   
                 internal_address_map = {socket_type: [f'tcp://localhost:{port}']}
                 external_address_map = dict()
 
+                if socket_type is not zmq.SUB:
+                    internal_address_map[zmq.SUB] = [f'tcp://localhost:{port+1}']
+
             elif t_type is TestNetworkElement.TransmissionTypes.EXT:
                 internal_address_map = dict()
                 external_address_map = {socket_type: [f'tcp://localhost:{port}']}
 
+                if socket_type is not zmq.SUB:
+                    external_address_map[zmq.SUB] = [f'tcp://localhost:{port+1}']
+
             network_config = NetworkConfig(network_name, internal_address_map, external_address_map)
             
-            super().__init__('RECEIVER', network_config, level)
+            super().__init__(name, network_config, level)
             self.socket_type = socket_type
-            self.msgs = []
             self.n = n
             self.t_type = t_type          
+
+        def activate(self) -> dict:
+            super().activate()
+
+            self.poller = azmq.Poller()
+            if self.t_type is TestNetworkElement.TransmissionTypes.INT:
+                for socket_type in self._internal_socket_map:
+                    socket, _ = self._internal_socket_map[socket_type]
+                    self.poller.register(socket, zmq.POLLIN)
+
+            elif self.t_type is TestNetworkElement.TransmissionTypes.EXT:
+                for socket_type in self._external_socket_map:
+                    socket, _ = self._external_socket_map[socket_type]
+                    self.poller.register(socket, zmq.POLLIN)
 
         async def routine(self):
             try:
                 for _ in tqdm (range (self.n), desc="RECEIVER:  Listening..."):
-                    self._log(f'waiting for incoming messages through port of type {self.socket_type}...')
-                    if self.t_type is TestNetworkElement.TransmissionTypes.INT:   
-                        dst, src, content = await self._receive_internal_msg(self.socket_type)
+                    self._log(f'waiting for incoming messages...')
 
-                    elif self.t_type is TestNetworkElement.TransmissionTypes.EXT:
-                        dst, src, content = await self._receive_external_msg(self.socket_type)
+                    socks = dict(await self.poller.poll())
 
+                    for polled_sock in socks:
+                        src, dst, content = None, None, None
+
+                        if self.t_type is TestNetworkElement.TransmissionTypes.INT:   
+                            for socket_type in self._internal_socket_map:
+                                sock, _ = self._internal_socket_map[socket_type]
+
+                                if polled_sock is sock:
+                                    dst, src, content = await self._receive_internal_msg(socket_type)
+                                    
+                                    if content['msg_type'] == 'KILL':
+                                        self._log(f'received kill message from {src} intended for {dst}! Reception status: {len(self.msgs)}/{self.n}')
+                                        return
+                                    break
+
+                        elif self.t_type is TestNetworkElement.TransmissionTypes.EXT:   # 
+                            for socket_type in self._external_socket_map:
+                                sock, _ = self._external_socket_map[socket_type]
+                            
+                                if polled_sock is sock:
+                                    dst, src, content = await self._receive_external_msg(socket_type)
+
+                                    if content['msg_type'] == 'KILL':
+                                        self._log(f'received kill message from {src} intended for {dst}! Reception status: {len(self.msgs)}/{self.n}')
+                                        return
+                                    break
+                        
+                        if content is not None:
+                            break
+                            
                     self.msgs.append(SimulationMessage(**content))
                     self._log(f'received a message from {src} intended for {dst}! Reception status: {len(self.msgs)}/{self.n}')
 
@@ -214,45 +286,86 @@ class TestNetworkElement(unittest.TestCase):
                             sender_port_type : zmq.SocketType, 
                             receiver_port_type : zmq.SocketType, 
                             port : int,
-                            n : int):
+                            n_receivers : int,
+                            n_messages : int,
+                            level : int = logging.INFO):
+
         if t_type is not TestNetworkElement.TransmissionTypes.INT and t_type is not TestNetworkElement.TransmissionTypes.EXT:
             raise TypeError('`t_type` must be of type `TransmissionTypes`')
 
-        sender = TestNetworkElement.Sender(t_type, sender_port_type, port, n)
-        receiver = TestNetworkElement.Receiver(t_type, receiver_port_type, port, n)
-
+        sender = TestNetworkElement.Sender(t_type, sender_port_type, port, n_messages, level)
+        receivers = []
+        for i in range(n_receivers):
+            receiver = TestNetworkElement.Receiver(f'RECEVER_{i+1}', t_type, receiver_port_type, port, n_messages, level)
+            receivers.append(receiver)
+        
         sender.activate()
-        receiver.activate()
+        for receiver in receivers:
+            receiver : TestNetworkElement.DummyNetworkElement
+            receiver.activate()
     
-        with concurrent.futures.ThreadPoolExecutor(2) as pool:
-            pool.submit(receiver.run, *[])
+        with concurrent.futures.ThreadPoolExecutor(len(receivers) + 1) as pool:
+            for receiver in receivers:
+                pool.submit(receiver.run, *[])
+            # print('\n')
             pool.submit(sender.run, *[])
+            # print('\n')
 
-        self.assertEqual(sender.msgs, receiver.msgs)  
-         
-    def test_external_messaging(self):
-        port = 5556
-        n = 10
+        if receiver_port_type is zmq.SUB:
+            received_messages = None
+            for receiver in receivers:
+                if received_messages is None:
+                    received_messages = receiver.msgs.copy()
+                
+                if received_messages != receiver.msgs:
+                    received_messages = []
+                    return
+        else:
+            received_messages = []
+            for receiver in receivers:
+                for msg in receiver.msgs:
+                    if msg not in received_messages:
+                        received_messages.append(msg)
 
-        # PUB-SUB pattern
-        print('\nTEST: External Message Broadcast (PUB-SUB)')
-        self.transmission_tester(TestNetworkElement.TransmissionTypes.EXT, zmq.PUB, zmq.SUB, port, n) 
-        print('\n')
+        self.assertEqual(len(sender.msgs), len(received_messages))
+        for msg in sender.msgs:
+            self.assertTrue(msg in received_messages)
 
-        # PUSH-PULL pattern
-        print('\nTEST: External Message Distribution (PUSH-PULL)')
-        self.transmission_tester(TestNetworkElement.TransmissionTypes.EXT, zmq.PUSH, zmq.PULL, port, n) 
-        print('\n')
+    def test_message_broadcast(self):
+        PORT = 5555
+        listeners = [1, 20]
+        N_MESSAGES = 20
 
-    def test_internal_messaging(self):
-        port = 5556
-        n = 10
-
-        # PUB-SUB pattern
+        # INTERNAL MESSAGING
         print('\nTEST: Internal Message Broadcast (PUB-SUB)')
-        self.transmission_tester(TestNetworkElement.TransmissionTypes.INT, zmq.PUB, zmq.SUB, port, n) 
+        for n_listeners in listeners:
+            print(f'\nNumber of listeners: {n_listeners}')
+            self.transmission_tester(TestNetworkElement.TransmissionTypes.INT, zmq.PUB, zmq.SUB, PORT, n_listeners, N_MESSAGES)
+            print('\n')
 
-        # PUSH-PULL pattern
+        # EXTERNAL MESSAGING
+        print('\nTEST: External Message Broadcast (PUB-SUB)')
+        for n_listeners in listeners:
+            print(f'\nNumber of listeners: {n_listeners}')
+            self.transmission_tester(TestNetworkElement.TransmissionTypes.EXT, zmq.PUB, zmq.SUB, PORT, n_listeners, N_MESSAGES)
+            print('\n')
+
+    def test_message_distribution(self):
+        PORT = 5555
+        listeners = [1, 20]
+        N_MESSAGES = 20
+
+        # INTERNAL MESSAGING
         print('\nTEST: Internal Message Distribution (PUSH-PULL)')
-        self.transmission_tester(TestNetworkElement.TransmissionTypes.INT, zmq.PUSH, zmq.PULL, port, n) 
-        print('\n')
+        for n_listeners in listeners:
+            print(f'Number of listeners: {n_listeners}\n')
+            self.transmission_tester(TestNetworkElement.TransmissionTypes.INT, zmq.PUSH, zmq.PULL, PORT, n_listeners, N_MESSAGES)
+            print('\n')
+
+        # EXTERNAL MESSAGING
+        print('\nTEST: External Message Broadcast (PUB-SUB)')
+        for n_listeners in listeners:
+            print(f'\nNumber of listeners: {n_listeners}')
+            self.transmission_tester(TestNetworkElement.TransmissionTypes.EXT, zmq.PUSH, zmq.PULL, PORT, n_listeners, N_MESSAGES)
+            print('\n')
+            
