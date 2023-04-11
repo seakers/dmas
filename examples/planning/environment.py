a9ba09b58f844d6b128484a13d597679aa274a69
Alan Aguilar <aguilaraj15@tamu.edu>
2023-04-11 17:12:09 -0500

    code clean-up

--- examples/planning/environment.py
+++ examples/planning/environment.py
@@ -102,6 +102,12 @@ class SimulationEnvironment(EnvironmentNode):
                         # toc message received
                         self.log(f"received message of type {content['msg_type']}. ending simulation...")
 
+                        # unpack message
+                        msg = TocMessage(**content)
+
+                        # update internal clock
+                        self.t = msg.t
+
                         # wait for all agent's to send their updated states
                         state_updates = await self.wait_for_agent_updates()
 
@@ -128,8 +134,8 @@ class SimulationEnvironment(EnvironmentNode):
         Waits for all agents to send in their state updates to the environment
         """
         try:
-            responses = dict()
-            while len(responses) < len(self._external_address_ledger):
+            updates = dict()
+            while len(updates) < len(self._external_address_ledger):
                 # read message from socket
                 _, src, content = await self.listen_peer_message()
                 self.log(f'agent message received: {content}')
@@ -139,7 +145,7 @@ class SimulationEnvironment(EnvironmentNode):
                     self.log(f"received message of type {content['msg_type']}. processing message....")
 
                     # unpack message
-                    responses[src] = AgentStateMessage(**content)
+                    updates[src] = AgentStateMessage(**content)
 
                     # send confirmation response
                     resp = NodeReceptionAckMessage(self.get_element_name(), src)
@@ -149,10 +155,10 @@ class SimulationEnvironment(EnvironmentNode):
                     self.log(f"received message of type {content['msg_type']}. ignoring message...")
                     resp = NodeReceptionIgnoredMessage(self.get_element_name(), src)
 
-                # respond to request
+                # respond to request messages
                 await self.respond_peer_message(resp)
             
-            return responses
+            return updates
 
         except asyncio.CancelledError as e:
             raise e
@@ -178,30 +184,6 @@ class SimulationEnvironment(EnvironmentNode):
 
         return range_updates
 
-    async def listen_for_manager(self):
-        try:
-            while True:
-                # do nothing. Manager is being listened to in `live()`
-                await asyncio.sleep(1e6)
-                # dst, src, content = await self._receive_manager_msg(zmq.SUB)
-                # self.log(f'message received: {content}', level=logging.DEBUG)
-
-                # if (dst in self.name 
-                #     and SimulationElementRoles.MANAGER.value in src 
-                #     and content['msg_type'] != ManagerMessageTypes.SIM_END.value):
-                    
-                #     self.log('simulation end message received! ending simulation...')
-                #     break
-                # else:
-                #     self.manager_broadcast_queue
-
-        except asyncio.CancelledError:
-            self.log(f'`listen_for_manager()` interrupted.')
-            return
-        except Exception as e:
-            self.log(f'`listen_for_manager()` failed. {e}')
-            raise e
-
     async def teardown(self) -> None:
         # nothing to tear-down
         return
@@ -225,25 +207,3 @@ class SimulationEnvironment(EnvironmentNode):
                 
         except asyncio.CancelledError:
             return
-
-if __name__ == '__main__':
-    port = 5555
-    network_name = 'TEST_NETWORK'
-    manager_network_config = NetworkConfig( network_name,
-											manager_address_map = {
-																	zmq.REP: [f'tcp://*:{port}'],
-																	zmq.PUB: [f'tcp://*:{port+1}'],
-																	zmq.PUSH: [f'tcp://localhost:{port+2}']})
-			
-    env_network_config = NetworkConfig( network_name,
-											manager_address_map = {
-													zmq.REQ: [f'tcp://localhost:{port}'],
-													zmq.SUB: [f'tcp://localhost:{port+1}'],
-													zmq.PUSH: [f'tcp://localhost:{port+2}']},
-											external_address_map = {
-													zmq.REP: [f'tcp://*:{port+3}'],
-													zmq.PUB: [f'tcp://*:{port+4}']
-											})
-
-    env = SimulationEnvironment(env_network_config,
-                                manager_network_config)
